d34328cd469e581161114db74c28b11bc7558a15
Oliver Hookins <ohookins@users.noreply.github.com>
2025-01-19 00:09:20 +0000

    Day11 WIP

---
+++ Day11/Part1.fs
@@ -0,0 +1,15 @@
+﻿module Day11.Part1
+
+open System.IO
+open System
+
+let parseInput (filename: string) =
+    File.Read
+    // File.ReadAllLines filename
+
+    // System.Int64.Parse
+
+let solve (filename: string): unit =
+    let stones = parseInput filename
+
+    printfn "%A" stones
