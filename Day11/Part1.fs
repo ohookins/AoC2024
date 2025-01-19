44fa4763f9bb32fb4955be420b656fccf1f05352
Oliver Hookins <ohookins@gmail.com>
2025-01-19 13:18:22 +1100

    Day 11 Part 1

--- Day11/Part1.fs
+++ Day11/Part1.fs
@@ -3,13 +3,39 @@
 open System.IO
 open System
 
-let parseInput (filename: string) =
-    File.Read
-    // File.ReadAllLines filename
+let ITERATIONS = 25
 
-    // System.Int64.Parse
+let parseInput (filename: string) =
+    File.ReadLines filename
+    |> Seq.map (fun line ->
+        line.Split(' ')
+        |> Seq.map (fun elem ->
+            System.Int64.Parse(elem)
+        )
+    )
+    |> Seq.head
+
+let blink (stones: seq<int64>) =
+    stones
+    |> Seq.map (fun stone ->
+        match stone with
+        | s when s = 0L -> seq { 1L }
+        | s when s.ToString().Length % 2 = 0 ->
+            let digits = s.ToString()
+            let length = digits.Length
+            let left = digits.Substring(0, length/2)
+            let right = digits.Substring(length/2, length/2)
+
+            seq { System.Int64.Parse(left); System.Int64.Parse(right)}
+        | _ -> seq { stone * 2024L }
+    )
+    |> Seq.collect id
 
 let solve (filename: string): unit =
     let stones = parseInput filename
+    let result =
+        Seq.init ITERATIONS (fun e -> 0L)
+        |> Seq.fold (fun acc _ -> blink acc) stones
+        |> Seq.length
 
-    printfn "%A" stones
+    printfn "%d" result
